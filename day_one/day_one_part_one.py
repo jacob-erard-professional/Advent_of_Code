e2b20dbef17d2f5b68219417e2573f89427f3ab6
Jacob Erard <jacob.a.erard@gmail.com>
2025-12-01 18:53:21 -0700

    Day one completed

---
+++ day_one/day_one_part_one.py
@@ -0,0 +1,19 @@
+def main(): 
+    inputs = []
+    with open("day_one/day_one_data.txt", "r") as file: 
+        for line in file: 
+            ins = line.strip() 
+            inputs.append([ins[:1], int(ins[1:])]) 
+        num = 50 
+        count = 0 
+        for dir, rot in inputs: 
+            if dir == "R":
+                num += rot 
+            else: 
+                num -= rot
+            num %= 100
+            if num == 0:
+                count += 1
+    print(count)
+if __name__ == "__main__": 
+    main()
---
+++ day_one/day_one_part_two.py
@@ -0,0 +1,24 @@
+def main(): 
+    inputs = []
+    with open("day_one/day_one_data.txt", "r") as file: 
+        for line in file: 
+            ins = line.strip() 
+            inputs.append([ins[:1], int(ins[1:])]) 
+        num = 50 
+        count = 0 
+        for dir, rot in inputs: 
+            curr = num 
+            if dir == "R":
+                num += rot 
+                count += num//100
+            else: 
+                num -= rot 
+                if curr == 0:
+                    count += abs(num)//100
+                else:
+                    count += (abs(num)//100 + 1) if num < 0 else 0
+                if num == 0:
+                    count += 1
+            num %= 100
+    print(count)
+main()
