d6409a0f37117ba5fdee42918c0948d5fc90301d
jacob-erard <jacob.a.erard@gmail.com>
2025-12-15 21:33:46 -0800

    Added comment

--- day_nine/day_nine_part_two.py
+++ day_nine/day_nine_part_two.py
@@ -61,6 +61,7 @@ def intersecting(first, second):
 area = 0
 for i in range(len(tiles)):
     for j in range(i + 1, len(tiles)):
+        # The biggest change is checking if it intersects ANY other line segment
         area = area if intersecting(tiles[i], tiles[j]) else max(area, (abs(tiles[i][0] - tiles[j][0])+1) * (abs(tiles[i][1] - tiles[j][1])+1))
 
 print(area)
